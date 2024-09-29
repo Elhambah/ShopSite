d6f73a56271a240d2b3bd7cc772f5ee622a1b940
Elham-Bahmani <elhambahmanitadavany@gmail.com>
2024-09-29 16:59:35 -0700

    update slider

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 
 let slideIndex = 1;
-function setSlide(index, input) {
+function setSlide(input, index) {
     slideIndex = index;
     let item = document.querySelector(`#${input}`);
-    let sildes = [...document.querySelector('.slides').children]
+    let sildes = [...document.querySelector('.slides').children];
+
     sildes.forEach((element) => {
         element.classList.remove('active');
     }
     )
     item.classList.add('active');
 }
+
+setInterval(() => {
+    slideIndex += 1;
+    if (slideIndex == 4) {
+        slideIndex = 1;
+    }
+    setSlide(`slid${slideIndex}`, slideIndex)
+}, 4000)
+
